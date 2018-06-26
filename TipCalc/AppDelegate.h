25f46e59d182cf828ef6af0aa25a0869794a25b7
Gisele Phalo <zellebelle@fb.com>
2018-06-26 14:45:28 -0700

    Initial Commit

---
+++ TipCalc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalc
+//
+//  Created by Gisele Phalo on 6/26/18.
+//  Copyright © 2018 Gisele Phalo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalc
+//
+//  Created by Gisele Phalo on 6/26/18.
+//  Copyright © 2018 Gisele Phalo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
